8db6aeadcdf41beae8d56c5159b58f109b469298
Nate Pisarski <nathanpisarski@gmail.com>
2015-10-15 22:44:41 -0400

    Abstract words slightly

--- src/access/strings.rs
+++ src/access/strings.rs
@@ -1,6 +1,11 @@
 //!Operations on all of Rust's String types (&str, String, etc)
 //! will go into the Strings library.
 
+// TODO: DOCUMENT / TEST
+pub fn to_string_vec(list: Vec<&str>) -> Vec<String> {
+	list.iter().map(|&c| {String::from(c)}).collect::<Vec<String>>()
+}
+
 pub fn to_words(line: String) -> Vec<String>{
-	line.split(' ').map(|c| {String::from(c)}).collect::<Vec<String>>()//TODO: Do tests, document, write examples
+	to_string_vec(line.split(' ').collect())
 }
